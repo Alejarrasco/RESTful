f39eee235277293ff5ec1e87f6b53842f431791d
Alejandro Carrasco <carrascoalejandro2003@gmail.com>
2023-08-30 09:43:34 -0400

    Siguiendo el tutorial de RESTful

---
+++ src/main/java/com/alecarrasco/restfulservice/controller/model/ProductID.java
@@ -0,0 +1,17 @@
+package com.alecarrasco.restfulservice.controller.model;
+
+public class ProductID {
+    
+    private final String id;
+
+    public ProductID(final String id) {
+        this.id = id;
+    }
+
+    //Getters
+
+    public String getId() {
+        return id;
+    }
+
+}
